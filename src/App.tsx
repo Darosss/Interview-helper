b3bb6c326d804a9722852480c2679640be59b01c
Darosss <daroing12@gmail.com>
2023-07-31 16:24:06 +0200

    feat: wrap ThemeProvider over whole app

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,29 @@ import { Home } from "./home";
 import { Route, Routes } from "react-router-dom";
 import { Questions } from "./questions";
 import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "./theme";
 
 export function App() {
   return (
     <>
-      <div className="app-wrapper">
-        <div className="menu-wrapper">
-          <Menu />
-          <ToastContainer autoClose={5000} theme="dark" />
-        </div>
-        <div className="content-wrapper">
-          <Routes>
-            <Route path="/">
-              <Route index element={<Home />} />
-              <Route path="questions">
-                <Route index element={<Questions />} />
+      <ThemeProvider>
+        <div className="app-wrapper">
+          <div className="menu-wrapper">
+            <Menu />
+            <ToastContainer autoClose={5000} theme="dark" />
+          </div>
+          <div className="content-wrapper">
+            <Routes>
+              <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="questions">
+                  <Route index element={<Questions />} />
+                </Route>
               </Route>
-            </Route>
-          </Routes>
+            </Routes>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
