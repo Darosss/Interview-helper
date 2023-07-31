0a938cca46e02d1133da12a1d4731c95983e06e3
Darosss <daroing12@gmail.com>
2023-07-31 16:23:34 +0200

    feat(component): add ToggleButton

---
+++ src/toggleButton/ToggleButton.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+interface ToggleButtonProps {
+  label: string;
+  toggled: boolean;
+  onClick: (value: boolean) => void;
+}
+
+export function ToggleButton({ label, toggled, onClick }: ToggleButtonProps) {
+  const [isToggled, toggle] = useState(toggled);
+
+  const callback = () => {
+    toggle(!isToggled);
+    onClick(!isToggled);
+  };
+  return (
+    <label className="toggle-button-wrapper">
+      <input type="checkbox" defaultChecked={isToggled} onClick={callback} />
+      <span />
+      <strong>{label}</strong>
+    </label>
+  );
+}
