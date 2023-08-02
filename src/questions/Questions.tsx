bd6c144c5425fd21a3d4b0da370b551bfd3fdd5a
Darosss <daroing12@gmail.com>
2023-08-02 17:36:39 +0200

    feat: add simple questions pagination

--- src/questions/Questions.tsx
+++ src/questions/Questions.tsx
@@ -1,59 +1,64 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { questionsList } from "./questionsList";
+import { getQuestionsListPaginated } from "./questionsList";
 import { QuestionData } from "./QuestionData";
-import { TopicsList } from "./questions.interface";
 import { QuestionsURLParams } from "./question.enums";
 import { QuestionsContext } from "./QuestionsContext";
+import { QuestionsListPaginated } from "./questions.interface";
+import ReactPaginate from "react-paginate";
 
 export function Questions() {
   const [URLSearchParams] = useSearchParams();
   const { doneQuestions } = useContext(QuestionsContext);
+  const [maxPages, setMaxPaxes] = useState(0);
+  const [page, setPage] = useState(0);
+  const [limit] = useState(10);
+  const [questionData, setQuestionData] = useState<QuestionsListPaginated[]>(
+    []
+  );
 
-  function getQuestions(
-    topics: TopicsList[],
-    topicId?: string,
-    random?: boolean,
-    shuffle?: boolean
-  ): JSX.Element | JSX.Element[] {
-    const topicsList = topics.filter(({ id }) => {
-      return topicId?.split(",").includes(id);
-    });
+  async function fetchData() {
+    const dataRes = getQuestionsListPaginated(
+      URLSearchParams.get(QuestionsURLParams.TOPIC_ID)?.split(",") ?? [],
+      doneQuestions,
+      !!URLSearchParams.get(QuestionsURLParams.RANDOM),
+      !!URLSearchParams.get(QuestionsURLParams.SHUFFLE),
+      { limit, page }
+    );
 
-    const filteredQuestionsList = topicsList
-      .map(({ questions, name }) =>
-        questions
-          .filter((item) => !doneQuestions.includes(item.id))
-          .map((item, idx) => (
-            <QuestionData key={`${idx}-${name}`} name={name} item={item} />
-          ))
-      )
-      .filter((item) => item !== undefined)
-      .flat();
-
-    if (random) {
-      return filteredQuestionsList[
-        Math.floor(Math.random() * filteredQuestionsList.length)
-      ];
-    }
-    if (shuffle) {
-      return filteredQuestionsList.sort(() => Math.random() - 0.5);
-    }
-
-    if (filteredQuestionsList.length > 0) return filteredQuestionsList;
-    return <>Choose topics to get questions </>;
+    setMaxPaxes(dataRes.maxPages);
+    setQuestionData(dataRes.data);
   }
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
 
   return (
     <>
       <div id="questions">
         <div className="questions-wrapper">
-          {getQuestions(
-            questionsList,
-            URLSearchParams.get(QuestionsURLParams.TOPIC_ID) ?? "",
-            !!URLSearchParams.get(QuestionsURLParams.RANDOM),
-            !!URLSearchParams.get(QuestionsURLParams.SHUFFLE)
-          )}
+          {questionData.map(({ name, id, question, answer }, idx) => (
+            <QuestionData
+              key={`${idx}-${name}`}
+              name={name}
+              item={{ id, question, answer }}
+            />
+          ))}
+        </div>
+        <div className="questions-pagination-wrapper prevent-select">
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={(e) => setPage(e.selected)}
+            pageRangeDisplayed={5}
+            pageCount={maxPages}
+            previousLabel="< previous"
+            renderOnZeroPageCount={null}
+            className="question-pagination"
+            activeLinkClassName="pagination-active"
+          />
         </div>
       </div>
     </>
--- src/questions/questionsList.ts
+++ src/questions/questionsList.ts
@@ -1,5 +1,44 @@
-import { TopicsList } from "./questions.interface";
+import { GetQuestionsListPaginated, TopicsList } from "./questions.interface";
 import { nodeQuestions, reactQuestions } from "./interviewQuestionsData";
+
+export function getQuestionsListPaginated(
+  topicsIds: string[],
+  doneQuestionsIds: string[],
+  random: boolean,
+  shuffle: boolean,
+  { limit, page }: { limit: number; page: number }
+): GetQuestionsListPaginated {
+  const choosenTopics = questionsList.filter(({ id }) => {
+    return topicsIds.includes(id);
+  });
+
+  let filteredQuestionsList = choosenTopics
+    .map(({ questions, name }) =>
+      questions
+        .filter((item) => !doneQuestionsIds.includes(item.id))
+        .map((item) => ({ name, ...item }))
+    )
+    .flat();
+
+  if (shuffle) {
+    filteredQuestionsList = filteredQuestionsList.sort(
+      () => Math.random() - 0.5
+    );
+  }
+
+  if (random) {
+    filteredQuestionsList = [
+      filteredQuestionsList[
+        Math.floor(Math.random() * filteredQuestionsList.length)
+      ],
+    ];
+  }
+
+  return {
+    data: filteredQuestionsList.splice(page * limit, limit),
+    maxPages: Math.ceil(filteredQuestionsList.length / limit) + 1,
+  };
+}
 
 export const questionsList = generateJSONQuestions([
   { name: "Node.js", questions: nodeQuestions },
