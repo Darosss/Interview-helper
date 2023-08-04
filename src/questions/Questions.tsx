8e59e5457b7dbdd4629098b1167433e5420ab90d
Darosss <daroing12@gmail.com>
2023-08-04 13:12:30 +0200

    refactor: move questions logic into context and remove it from Questions component

--- src/questions/Questions.tsx
+++ src/questions/Questions.tsx
@@ -1,45 +1,16 @@
-import { useContext, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { getQuestionsListPaginated } from "./questionsList";
+import { useContext } from "react";
 import { QuestionData } from "./QuestionData";
-import { QuestionsURLParams } from "./question.enums";
 import { QuestionsContext } from "./QuestionsContext";
-import { QuestionsListPaginated } from "./questions.interface";
 import ReactPaginate from "react-paginate";
-
 export function Questions() {
-  const [urlSearchParams] = useSearchParams();
-  const { doneQuestions } = useContext(QuestionsContext);
-  const [maxPages, setMaxPaxes] = useState(0);
-  const [page, setPage] = useState(0);
-  const [limit] = useState(10);
-  const [questionData, setQuestionData] = useState<QuestionsListPaginated[]>(
-    []
-  );
-
-  async function fetchData() {
-    const dataRes = getQuestionsListPaginated(
-      urlSearchParams.get(QuestionsURLParams.TOPIC_ID)?.split(",") ?? [],
-      doneQuestions,
-      !!urlSearchParams.get(QuestionsURLParams.RANDOM),
-      !!urlSearchParams.get(QuestionsURLParams.SHUFFLE),
-      { limit, page }
-    );
-
-    setMaxPaxes(dataRes.maxPages);
-    setQuestionData(dataRes.data);
-  }
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page, urlSearchParams]);
+  const { questionsData, fetchQuestionsData, maxPages } =
+    useContext(QuestionsContext);
 
   return (
     <>
       <div id="questions">
         <div className="questions-wrapper">
-          {questionData.map(({ name, id, question, answer }, idx) => (
+          {questionsData.map(({ name, id, question, answer }, idx) => (
             <QuestionData
               key={`${idx}-${name}`}
               name={name}
@@ -52,7 +23,9 @@ export function Questions() {
             <ReactPaginate
               breakLabel="..."
               nextLabel="next >"
-              onPageChange={(e) => setPage(e.selected)}
+              onPageChange={(e) => {
+                fetchQuestionsData(e.selected);
+              }}
               pageRangeDisplayed={5}
               pageCount={maxPages}
               previousLabel="< previous"
--- src/questions/QuestionsContext.tsx
+++ src/questions/QuestionsContext.tsx
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { LocalStorageNames } from "../localstorage.enum";
+import { QuestionsListPaginated } from "./questions.interface";
+import { QuestionsURLParams } from "./question.enums";
+import { useSearchParams } from "react-router-dom";
+import { getQuestionsListPaginated } from "./questionsList";
 
 interface QuestionsContextData {
   doneQuestions: string[];
   updateDoneQuestions: (value: string[]) => void;
+  questionsData: QuestionsListPaginated[];
+  fetchQuestionsData: (page?: number) => Promise<void>;
+  maxPages: number;
 }
 
 const initialQuestionsState = {
   doneQuestions:
     localStorage.getItem(LocalStorageNames.doneQuestions)?.split(",") || [],
   updateDoneQuestions: () => {},
+  questionsData: [],
+  fetchQuestionsData: async () => {},
+  maxPages: 0,
 };
 
 export const QuestionsContext = React.createContext<QuestionsContextData>(
@@ -17,8 +27,16 @@ export const QuestionsContext = React.createContext<QuestionsContextData>(
 );
 
 export function QuestionsProvider({ children }: { children: React.ReactNode }) {
+  const [urlSearchParams] = useSearchParams();
   const [doneQuestions, setQuestions] = useState<string[]>(
     initialQuestionsState.doneQuestions
+  );
+  const [limit] = useState(10);
+  const [maxPages, setMaxPaxes] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const [questionsData, setQuestionsData] = useState<QuestionsListPaginated[]>(
+    []
   );
 
   useEffect(() => {
@@ -26,14 +44,44 @@ export function QuestionsProvider({ children }: { children: React.ReactNode }) {
       LocalStorageNames.doneQuestions,
       doneQuestions.join(",")
     );
+
+    fetchQuestionsData(currentPage);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [doneQuestions]);
+
+  async function fetchQuestionsData(page?: number) {
+    const dataRes = getQuestionsListPaginated(
+      urlSearchParams.get(QuestionsURLParams.TOPIC_ID)?.split(",") ?? [],
+      doneQuestions,
+      !!urlSearchParams.get(QuestionsURLParams.RANDOM),
+      !!urlSearchParams.get(QuestionsURLParams.SHUFFLE),
+      { limit, page: page ?? currentPage }
+    );
+
+    if (page) setCurrentPage(page);
+    setMaxPaxes(dataRes.maxPages);
+    setQuestionsData(dataRes.data);
+  }
+
+  useEffect(() => {
+    fetchQuestionsData(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [urlSearchParams]);
 
   function updateDoneQuestions(newDoneQuestions: string[]) {
     setQuestions([...newDoneQuestions]); // Create a new array with the updated values
   }
 
   return (
-    <QuestionsContext.Provider value={{ doneQuestions, updateDoneQuestions }}>
+    <QuestionsContext.Provider
+      value={{
+        doneQuestions,
+        updateDoneQuestions,
+        questionsData,
+        fetchQuestionsData,
+        maxPages,
+      }}
+    >
       {children}
     </QuestionsContext.Provider>
   );
